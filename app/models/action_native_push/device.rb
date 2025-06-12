cc1b1b9a844aaa738cc6c9145964b2644fe93a31
Jacopo Beschi <jacopobeschi@37signals.com>
2025-06-12 10:18:22 +0200

    Explanatory comment

--- app/models/action_native_push/device.rb
+++ app/models/action_native_push/device.rb
@@ -4,6 +4,7 @@ module ActionNativePush
   class Device < ApplicationRecord
     belongs_to :record, polymorphic: true, optional: true
 
+    # E.g. { "ios" => "ios", "android" => "android" }
     enum :platform, ActionNativePush.configuration.supported_platforms.index_with(&:itself), validate: true
 
     validates_presence_of :token
