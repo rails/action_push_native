8f7d4da23b254a91249b03d94d7d7994b724c30e
Jacopo Beschi <jacopobeschi@37signals.com>
2025-06-13 14:30:13 +0200

    More lenient just use validation instead of an enum

--- app/models/action_native_push/device.rb
+++ app/models/action_native_push/device.rb
@@ -4,10 +4,8 @@ module ActionNativePush
   class Device < ApplicationRecord
     belongs_to :record, polymorphic: true, optional: true
 
-    # E.g. { "ios" => "ios", "android" => "android" }
-    enum :application, ActionNativePush.configuration.supported_applications.index_with(&:itself), validate: true
-
     validates_presence_of :token
+    validates :application, inclusion: { in: ActionNativePush.configuration.supported_applications }
 
     def on_token_error
       destroy!
