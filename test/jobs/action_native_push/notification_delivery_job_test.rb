32b7d185eb51b26cf6d772f864dbcc8278d3efbd
Jacopo Beschi <jacopobeschi@37signals.com>
2025-06-23 14:31:33 +0200

    Add tests for exponential backoff retry

---
+++ test/jobs/action_native_push/notification_delivery_job_test.rb
@@ -0,0 +1,30 @@
+require "test_helper"
+
+module ActionNativePush
+  class NotificationDeliveryJobTest < ActiveSupport::TestCase
+    test "429 errors are retried with an exponential backoff delay" do
+      device = action_native_push_devices(:iphone)
+      Notification.any_instance.stubs(:deliver_to).raises(Errors::TooManyRequestsError)
+
+      assert_enqueued_jobs 1, only: ActionNativePush::NotificationDeliveryJob do
+        ActionNativePush::NotificationDeliveryJob.perform_later({}, device)
+      end
+
+      [ 1, 2, 4, 8, 16 ].each do |minutes|
+        perform_enqueued_jobs only: ActionNativePush::NotificationDeliveryJob
+        assert_wait minutes.minutes
+      end
+
+      Notification.any_instance.stubs(:deliver_to)
+      ActionNativePush::NotificationDeliveryJob.perform_now({}, device)
+      perform_enqueued_jobs only: ActionNativePush::NotificationDeliveryJob
+    end
+
+    private
+      def assert_wait(seconds)
+        job = enqueued_jobs_with(only: ActionNativePush::NotificationDeliveryJob).last
+        delay = job["scheduled_at"].to_time - job["enqueued_at"].to_time
+        assert_in_delta seconds, delay, 0.15 * delay, "Expected job to wait approximately #{seconds} seconds, but waited #{delay} seconds instead."
+      end
+  end
+end
