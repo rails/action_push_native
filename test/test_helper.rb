6c9e54d358ea0761692e98f0c7ec96f503406aba
Jacopo Beschi <jacopobeschi@37signals.com>
2025-07-10 17:02:14 +0200

    Not needed

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,7 +9,6 @@ require "webmock/minitest"
 # Load fixtures from the engine
 if ActiveSupport::TestCase.respond_to?(:fixture_paths=)
   ActiveSupport::TestCase.fixture_paths = [ File.expand_path("fixtures", __dir__) ]
-  ActionDispatch::IntegrationTest.fixture_paths = ActiveSupport::TestCase.fixture_paths
   ActiveSupport::TestCase.file_fixture_path = File.expand_path("fixtures", __dir__) + "/files"
   ActiveSupport::TestCase.fixtures :all
 end
