c9de63accfec1338e5853b63376db0b52b4d072c
Jacopo Beschi <jacopobeschi@37signals.com>
2025-07-25 14:25:13 +0200

    FCM: OpenSSL connect errors are retried

--- test/lib/action_native_push/service/fcm_test.rb
+++ test/lib/action_native_push/service/fcm_test.rb
@@ -39,15 +39,18 @@ module ActionNativePush
       test "push response error" do
         stub_request(:post, "https://fcm.googleapis.com/v1/projects/your_project_id/messages:send").
           to_return(status: 503, body: { error: { message: "Bad Request" } }.to_json)
-
         assert_raises ActionNativePush::Errors::ServiceUnavailableError do
           @fcm.push(@notification)
         end
 
         stub_request(:post, "https://fcm.googleapis.com/v1/projects/your_project_id/messages:send").
           to_return(status: 400, body: { error: { message: "message is too big" } }.to_json)
-
         assert_raises ActionNativePush::Errors::PayloadTooLargeError do
+          @fcm.push(@notification)
+        end
+
+        Net::HTTP.stubs(:start).raises(OpenSSL::SSL::SSLError.new("SSL_connect returned=1 errno=0 state=error"))
+        assert_raises ActionNativePush::Errors::ConnectionError do
           @fcm.push(@notification)
         end
       end
