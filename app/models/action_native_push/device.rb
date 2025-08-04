3e170386ceaf12db04da1feef4491f973a66c351
Jacopo Beschi <jacopobeschi@37signals.com>
2025-08-04 09:18:49 +0200

    Device: Drop token validation

--- app/models/action_native_push/device.rb
+++ app/models/action_native_push/device.rb
@@ -4,7 +4,6 @@ module ActionNativePush
   class Device < ApplicationRecord
     belongs_to :owner, polymorphic: true, optional: true
 
-    validates_presence_of :token
     validates :application, inclusion: { in: ActionNativePush.supported_applications }
 
     def on_token_error
