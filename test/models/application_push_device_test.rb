ea9b100ff878042c4773e3c1730a6b43d0ea5e35
Jacopo Beschi <jacopobeschi@37signals.com>
2025-08-11 11:16:57 +0200

    New line

--- test/models/application_push_device_test.rb
+++ test/models/application_push_device_test.rb
@@ -5,6 +5,7 @@ class ApplicationPushDeviceTest < ActiveSupport::TestCase
     notification = ActionPush::Notification.new(title: "Hi!")
     iphone = application_push_devices(:iphone_6)
     ActionPush::Service::Apns.any_instance.expects(:push).raises(ActionPush::TokenError)
+
     assert_no_difference -> { ApplicationPushDevice.count } do
       iphone.push(notification)
     end
