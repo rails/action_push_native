663b8b9ec43dfd842acb93a6d7408f035b677c75
Jacopo Beschi <jacopobeschi@37signals.com>
2025-08-12 12:43:30 +0200

    No longer needed

--- lib/action_push/notification/builder.rb
+++ lib/action_push/notification/builder.rb
@@ -18,36 +18,27 @@ module ActionPush::Notification::Builder
 
   class_methods do
     def with_data(data)
-      new_class_preserving_name(self) do
+      Class.new(self) do
         self.default_data = self.default_data.merge data
       end
     end
 
     def with_apple(apple_data)
-      new_class_preserving_name(self) do
+      Class.new(self) do
         self.default_apns_payload = self.default_apns_payload.merge apple_data
       end
     end
 
     def with_google(google_data)
-      new_class_preserving_name(self) do
+      Class.new(self) do
         self.default_fcm_payload = self.default_fcm_payload.merge google_data
       end
     end
 
     def silent
-      new_class_preserving_name(self) do
+      Class.new(self) do
         self.default_high_priority = false
       end.with_apple(content_available: 1)
     end
-
-    private
-      def new_class_preserving_name(klass, &block)
-        Class.new(klass, &block).tap do |notification_class|
-          notification_class.define_singleton_method(:name) do
-            klass.name
-          end
-        end
-      end
   end
 end
